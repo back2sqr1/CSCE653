9739c4a579e1f21f151429c6216a8f72553a488f
David Zhang <theredhairedyonko@gmail.com>
2025-01-31 21:23:57 -0800

    cleanup

--- PageRank/p1.jl
+++ PageRank/p1.jl
@@ -30,50 +30,11 @@ function generate_graph(n)
 end
 
 # Auto gen graph
-# A = generate_graph(20)
-# A = sparse(A)
-# # generate x y coordinates for the nodes from random numbers from 1..100
-# xy = rand(20,2) * 100
-
-# Load the graph from a file 
-filename_nodes = "PageRank/data/fb-pages-public-figure/fb-pages-public-figure.nodes"
-filename_edges = "PageRank/data/fb-pages-public-figure/fb-pages-public-figure.edges"
-
-# Read the nodes
-nodes = open(filename_nodes) do file
-    readlines(file)
-end
-# remove the first Line
-nodes = nodes[2:end]
-
-# Read the edges
-edges = open(filename_edges) do file
-    readlines(file)
-end
-
-# Create a adjacency matrix from the edges 
-# nodes come in the form id,name,new_id
-# edges come in the form id1,id2
-
-# only take top 1000 nodes
-nodes = nodes[1:1000]
-
-# If your data is zero-indexed, adjust the matrix size accordingly:
-n = 1000
-A = spzeros(n, n)
-# only takes edges with 1...1000
-
-for edge in edges
-    edge = split(edge, ",")
-    id1 = parse(Int, edge[1]) + 1
-    id2 = parse(Int, edge[2]) + 1
-    if id1 <= 1000 && id2 <= 1000
-        A[id1, id2] = 1
-    end
-end
-
-xy = rand(n,2)
-
+A = generate_graph(20)
+A = sparse(A)
+# generate x y coordinates for the nodes from random numbers from 1..100
+xy = rand(20,2) * 100
+n = 20
 
 
 A.colptr
@@ -94,6 +55,8 @@ M = alp * P + (1 - alp) * v_et # matrix M
 RHS = (1 - alp) * v # right hand side of the equation
 LHS = (I - alp * P) # left hand side of the equation
 x = round.(LHS\RHS,digits=2) # solve the equation 
+f = display_graph(A,xy)
+
 for i = 1:n
     annotate!(f,xy[i,1],xy[i,2]-1.5, text("$(x[i])", :red, 10))
 end
--- PageRank/p2.jl
+++ PageRank/p2.jl
@@ -21,35 +21,14 @@ end
 # remove the first Line
 nodes = nodes[2:end]
 
-# map the nodes to their ids
-node_map = Dict()
-node_list = []
-for (i, node) in enumerate(nodes)
-    node = split(node, ",")
-    push!(node_list, node[])
-end
-
-
 # Read the edges
 edges = open(filename_edges) do file
     readlines(file)
 end
-
-# Create a adjacency matrix from the edges 
-# nodes come in the form id,name,new_id
-# edges come in the form id1,id2
-
-# only take top 1000 nodes
-
-
-
-# If your data is zero-indexed, adjust the matrix size accordingly:
 n = 1000
 A = spzeros(n, n)
+
 # only takes edges with 1...1000
-
-
-
 for edge in edges
     edge = split(edge, ",")
     id1 = parse(Int, edge[1]) + 1
@@ -57,13 +36,11 @@ for edge in edges
     if id1 <= 1000 && id2 <= 1000
         A[id1, id2] = 1
         A[id2, id1] = 1
-        println("id1: $id1, id2: $id2")
     end
 end
 # Check that all nodes have edges
 for i in 1:n
     if sum(A[i, :]) == 0
-        println("Node $i has no edges")
         # add random edges
         for j in 1:n
             if i != j && rand() < 0.1
@@ -92,7 +69,6 @@ Dinv = spdiagm(dinv) # diagonal matrix of the inverse of the sum of the columns
 P = A'*Dinv # transition matrix
 x = ones(n) / n # PageRank vector
 rdata = []
-labels = []
 for a in [0.1, 0.5, 0.9]
     x = ones(n)/n
     rd = []
@@ -101,7 +77,6 @@ for a in [0.1, 0.5, 0.9]
         x = a * P * x + (1-a) * v
     end
     push!(rdata, rd)
-    push!(labels, "alpha = $a")
 end
 
 plt = plot(title="Residual Norm vs. Iteration", xlabel="Iteration", ylabel="Residual Norm", legend=:topright)
